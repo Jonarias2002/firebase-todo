263fc9057258bc4c7141aaac0f58a699d954387d
Jonathan <jonajam2002@gmail.com>
2023-02-24 00:17:36 -0400

    cambio de id a clases

--- src/app/logout.js
+++ src/app/logout.js
@@ -1,7 +1,7 @@
 import { signOut } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"
 import { auth } from "./firebase.js";
 
-const logout = document.querySelector("#logout");
+const logout = document.querySelector(".logout");
 
 logout.addEventListener("click", async (e) => {
   e.preventDefault();
